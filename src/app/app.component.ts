e3aafb59e1b46617c8c75febb30963e17564bb76
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2025-03-14 00:43:05 +0300

    Create First Model-and-Test With-Product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './shared/Models/Product';
+import { IPagnation } from './shared/Models/Pagnation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  baseURL = 'https://localhost:7184/api/Products/get-all';
+  Product:IProduct[];
+  constructor(private http:HttpClient) {
+
+  }
+  getProduct(){
+    return this.http.get(this.baseURL).subscribe({
+      next:((value:IPagnation)=>{
+       this.Product=value.data
+        console.log(value.data)
+      })
+    })
+  }
+  ngOnInit(): void {
+    this.getProduct() 
+  }
   title = 'client';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,10 +14,12 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    SharedModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideHttpClient(withFetch())
   ],
   bootstrap: [AppComponent]
 })
