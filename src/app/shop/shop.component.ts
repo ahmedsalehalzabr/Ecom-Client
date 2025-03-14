9cec1e59e4495d85303474043cd7633b1f1b68af
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2025-03-15 01:23:49 +0300

    Complete filtration by Category id

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -12,23 +12,29 @@ import { ICateogry } from '../shared/Models/Category';
 export class ShopComponent implements OnInit{
   product:IProduct[];
   Category: ICateogry[];
+  CategoryId:number
   constructor(private shopService:ShopService){}
   ngOnInit(): void {
       this.getAllProduct();
       this.getCategory();
   }
 getAllProduct(){
-  this.shopService.getProduct().subscribe({
+  this.shopService.getProduct(this.CategoryId).subscribe({
     next:((value:IPagnation)=> {
           this.product=value.data
     })
   })
 }
+
 getCategory() {
   this.shopService.getCategory().subscribe({
     next: (value) => {
       this.Category = value;
     },
   });
+}
+SelectedId(categoryid:number){
+  this.CategoryId=categoryid
+  this.getAllProduct();
 }
 }
--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IPagnation } from '../shared/Models/Pagnation';
 import { ICateogry } from '../shared/Models/Category';
@@ -10,8 +10,12 @@ export class ShopService {
   baseURL = 'https://localhost:7184/api/';
 
   constructor(private http:HttpClient) { }
-  getProduct(){
-    return this.http.get<IPagnation>(this.baseURL+"Products/get-all")
+  getProduct(CategoryId?:number){
+    let parm=new HttpParams();
+    if(CategoryId){
+      parm=parm.append("categoryId",CategoryId)
+    }
+    return this.http.get<IPagnation>(this.baseURL+"Products/get-all",{params:parm})
   }
   getCategory(){
     return this.http.get<ICateogry[]>(this.baseURL+"Categories/get-all")
